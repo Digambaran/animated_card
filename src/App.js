9965b66af162f27e77b30f0232fcfaf90fdd8b2c
Digambaran <arjunsreekumar.1@gmail.com>
2022-09-30 10:44:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Animated_card from './animated_card'
+export default function App() {
+  return (
+    <div>
+      <Animated_card/>
+    </div>
+  )
+}
---
+++ src/animated_card.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Animated_card = () => {
+  return (
+   <p>Hello from animated_card </p>
+  )
+}
+export default Animated_card
